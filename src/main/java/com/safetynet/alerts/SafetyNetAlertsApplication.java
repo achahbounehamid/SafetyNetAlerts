2cc60197c76650001dd275690a86aaa6eae3c50d
achahbounehamid <hamid.achahboune@gmail.com>
2024-12-24 11:15:29 +0100

    Ajouter les modification

--- src/main/java/com/safetynet/alerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/safetynet/alerts/SafetyNetAlertsApplication.java
@@ -2,13 +2,20 @@ package com.safetynet.alerts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"com.safetynet.alerts.controller",
+//		"com.safetynet.alerts.service",
+//		"com.safetynet.alerts.repository",
+//		"com.safetynet.alerts.model",
+//		"com.safetynet.alerts.dto"})
 public class SafetyNetAlertsApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(SafetyNetAlertsApplication.class, args);
+		System.out.println("L'application SafetyNet Alerts a démarré avec succès !");
 	}
 
 }
